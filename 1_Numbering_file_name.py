509724b8eef2922925b7eaea4958e9fbf1d5fdd6
Taeho Shin <120359150+hotelshoe27@users.noreply.github.com>
2023-01-26 11:01:02 +0900

    Updates

--- 1_Numbering_file_name.py
+++ 1_Numbering_file_name.py
@@ -1,13 +1,15 @@
 import os
 
-foler_path = "./data/climb" #-- 파일이 저장된 폴더 경로
+foler_path = "./data/climb"
+#foler_path = "./data/walk"
 folderlist = os.listdir(foler_path)
 
 i = 1
 
 for name in folderlist:
     src = os.path.join(foler_path, name)
-    dst = 'climbing' + str(i) + '.png' #-- 확장자의 경우 필요에 따라 변경할 것
+    dst = 'climbing' + str(i) + '.png'
+    #dst = 'walking' + str(i) + '.png'
     dst = os.path.join(foler_path, dst)
     os.rename(src,dst)
     i += 1
